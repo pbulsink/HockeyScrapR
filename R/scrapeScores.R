fbffc9482350baef6b6480b1d3ec0bc868073cf7
Philip Bulsink <philip.bulsink@uottawa.ca>
2017-01-09 12:57:53 -0500

    And scores too.

--- R/scrapeScores.R
+++ R/scrapeScores.R
@@ -366,7 +366,9 @@ scrapeScores <- function(data_dir = "./data/scores/", ...) {
 #' @examples
 #' \dontrun{updateScores()}
 #' \dontrun{updatePlayers(data_dir = "./data/", years_back=2}
+updateScores <- function(score_data, data_dir = "./data/scores/", ...) {
     last_score_date<-max(score_data$Date)
+    getAndSaveNHLGames(data_dir = data_dir, start = as.numeric(format(last_score_date, "%Y")), ...)
     hockey_data <- readHockeyData(data_dir = data_dir, ...)
     saveRDS(hockey_data, paste0(data_dir, "scores-", Sys.Date(), ".RDS"))
     return(hockey_data)
