8d4467a638b2f3ae72f6030637e0eb960734f7d8
Philip Bulsink <pbulsink@users.noreply.github.com>
2017-01-19 17:27:03 -0500

    Dev - Get Schedule  (#3)
    * Work on an 'updatePlayers' function
    
    * Additional work on update scripts
    
    * And scores too.
    
    * First bit of test code.
    
    * Forgot to tell the package to load test that
    
    * Tighter scrape file handling
    
    * Some testing on scrapePlayers
    
    * More Tests
    
    * Fixed failing tests
    
    * More tests (likely fails)
    
    * More tests work
    
    * Thinning Helper file from unneeded data
    
    * Tests for score scraping (likely fails)
    
    * score scraping works.
    
    * 'cool tool bro' get current season
    
    * Test the current season getter
    
    * Some tests for scraping Rosters
    
    * tests for score updating
    
    * Tidied code, and more tests for scraping scores
    
    * Get Current Season function & test
    
    * Fixed test for schedule.
    
    * Version Bump 0.1.2

--- tests/testthat/test-scrapeScores.R
+++ tests/testthat/test-scrapeScores.R
@@ -4,7 +4,6 @@ source("helper-scrapeScores.R")
 test_that("Testing scraping NHL scores", {
   expect_false(getAndSaveNHLGames(start = 2005, end = 2005))
   expect_message(getAndSaveNHLGames(start = 2005, end = 2005), "Can't collect 2004-2005. No season due to lockout. Not collecting any data.")
-  
   expect_true(getAndSaveNHLGames(start = 2013, end = 2013, data_dir = "./", sleep = 10))
   f1 <- "./20122013.csv"
   f2 <- "./20122013Playoffs.csv"
@@ -12,8 +11,9 @@ test_that("Testing scraping NHL scores", {
   expect_true(file.exists(f2))
   try_delete(f1)
   try_delete(f2)
-  
-  expect_message(getAndSaveNHLGames(start = 2013, end = 2012, data_dir = "./", sleep = 10), "Start must be less than end. Reversing values.")
+
+  expect_message(getAndSaveNHLGames(start = 2013, end = 2012, data_dir = "./",
+    sleep = 10), "Start must be less than end. Reversing values.")
   f1 <- "./20112012.csv"
   f2 <- "./20112012Playoffs.csv"
   f3 <- "./20122013.csv"
@@ -26,45 +26,49 @@ test_that("Testing scraping NHL scores", {
   try_delete(f2)
   try_delete(f3)
   try_delete(f4)
-  
-  expect_message(getAndSaveNHLGames(start = 2005, end = 2006, data_dir = "./", sleep = 10), "Can't collect 2004-2005. No season due to lockout. Collecting from 2005-2006 to there.")
+
+  expect_message(getAndSaveNHLGames(start = 2005, end = 2006, data_dir = "./",
+    sleep = 10), "Can't collect 2004-2005. No season due to lockout. Collecting from 2005-2006 to there.")
   f1 <- "./20052006.csv"
   f2 <- "./20052006Playoffs.csv"
   expect_true(file.exists(f1))
   expect_true(file.exists(f2))
   try_delete(f1)
   try_delete(f2)
-  
-  expect_message(getAndSaveNHLGames(start = 2004, end = 2005, data_dir = "./", sleep = 10), "Can't collect 2004-2005. No season due to lockout. Collecting to 2003-2004.")
+
+  expect_message(getAndSaveNHLGames(start = 2004, end = 2005, data_dir = "./",
+    sleep = 10), "Can't collect 2004-2005. No season due to lockout. Collecting to 2003-2004.")
   f1 <- "./20032004.csv"
   f2 <- "./20032004Playoffs.csv"
   expect_true(file.exists(f1))
   expect_true(file.exists(f2))
   try_delete(f1)
   try_delete(f2)
-  
-  expect_message(getAndSaveNHLGames(start = 1917, end = 1918, data_dir = "./", sleep = 10), "NHL started in 1917-1918. Collecting data from there forwards.")
+
+  expect_message(getAndSaveNHLGames(start = 1917, end = 1918, data_dir = "./",
+    sleep = 10), "NHL started in 1917-1918. Collecting data from there forwards.")
   f1 <- "./19171918.csv"
   f2 <- "./19171918Playoffs.csv"
   expect_true(file.exists(f1))
   expect_true(file.exists(f2))
   try_delete(f1)
   try_delete(f2)
-  
-  expect_message(getAndSaveNHLGames(start = 2018, end = 2019, data_dir = "./", sleep = 10), "Can't start collecting data past next season. Collecting final season.")
+
+  expect_message(getAndSaveNHLGames(start = 2018, end = 2019, data_dir = "./",
+    sleep = 10), "Can't start collecting data past next season. Collecting final season.")
   f1 <- "./20162017.csv"
   expect_true(file.exists(f1))
   try_delete(f1)
-  
-  expect_message(getAndSaveNHLGames(start = 2017, end = 2019, data_dir = "./", sleep = 10), "Can't collect past this season. Collecting up to there.")
+
+  expect_message(getAndSaveNHLGames(start = 2017, end = 2019, data_dir = "./",
+    sleep = 10), "Can't collect past this season. Collecting up to there.")
   f1 <- "./20162017.csv"
   expect_true(file.exists(f1))
   try_delete(f1)
-  
+
 })
 
 test_that("Testing scraping WHA scores", {
-  
   expect_true(getAndSaveWHAGames(start = 1975, end = 1975, data_dir = "./", sleep = 10))
   f1 <- "./wha19741975.csv"
   f2 <- "./wha19741975Playoffs.csv"
@@ -72,8 +76,9 @@ test_that("Testing scraping WHA scores", {
   expect_true(file.exists(f2))
   try_delete(f1)
   try_delete(f2)
-  
-  expect_message(getAndSaveWHAGames(start = 1975, end = 1974, data_dir = "./", sleep = 10), "Start must be less than end. Reversing values.")
+
+  expect_message(getAndSaveWHAGames(start = 1975, end = 1974, data_dir = "./",
+    sleep = 10), "Start must be less than end. Reversing values.")
   f1 <- "./wha19731974.csv"
   f2 <- "./wha19731974Playoffs.csv"
   f3 <- "./wha19741975.csv"
@@ -86,24 +91,27 @@ test_that("Testing scraping WHA scores", {
   try_delete(f2)
   try_delete(f3)
   try_delete(f4)
-  
-  expect_message(getAndSaveWHAGames(start = 1972, end = 1973, data_dir = "./", sleep = 10), "WHA started in 1972-1973. Collecting data from there forwards.")
+
+  expect_message(getAndSaveWHAGames(start = 1972, end = 1973, data_dir = "./",
+    sleep = 10), "WHA started in 1972-1973. Collecting data from there forwards.")
   f1 <- "./wha19721973.csv"
   f2 <- "./wha19721973Playoffs.csv"
   expect_true(file.exists(f1))
   expect_true(file.exists(f2))
   try_delete(f1)
   try_delete(f2)
-  
-  expect_message(getAndSaveWHAGames(start = 1980, end = 1981, data_dir = "./", sleep = 10), "WHA ended in 1978-1979. Collecting final season.")
+
+  expect_message(getAndSaveWHAGames(start = 1980, end = 1981, data_dir = "./",
+    sleep = 10), "WHA ended in 1978-1979. Collecting final season.")
   f1 <- "./wha19781979.csv"
   f2 <- "./wha19781979Playoffs.csv"
   expect_true(file.exists(f1))
   expect_true(file.exists(f2))
   try_delete(f1)
   try_delete(f2)
-  
-  expect_message(getAndSaveWHAGames(start = 1979, end = 1981, data_dir = "./", sleep = 10), "WHA ended in 1978-1979. Collecting up to there.")
+
+  expect_message(getAndSaveWHAGames(start = 1979, end = 1981, data_dir = "./",
+    sleep = 10), "WHA ended in 1978-1979. Collecting up to there.")
   f1 <- "./wha19781979.csv"
   f2 <- "./wha19781979Playoffs.csv"
   expect_true(file.exists(f1))
@@ -125,12 +133,11 @@ test_that("Reading Hockey Data Works", {
   expect_true(file.exists(f2))
   expect_true(file.exists(f3))
   expect_true(file.exists(f4))
-  
-  scores_data <- readHockeyData(data_dir = "./", nhl_year_list = c(2013), wha_year_list = c(1973), 
+
+  scores_data <- readHockeyData(data_dir = "./", nhl_year_list = c(2013), wha_year_list = c(1973),
     playoffs = TRUE, lastPlayoffs = TRUE)
-  
+
   expect_equivalent(scores_data, cleaned_scores)
-  
   try_delete(f1)
   try_delete(f2)
   try_delete(f3)
@@ -149,16 +156,16 @@ test_that("Scores Update", {
 })
 
 test_that("Scraper works", {
-  scraped_scores <- scrapeScores(data_dir = "./", start = 1976, end = 1976, nhl_year_list = 1976, 
+  scraped_scores <- scrapeScores(data_dir = "./", start = 1976, end = 1976, nhl_year_list = 1976,
     wha_year_list = 1976, playoffs = TRUE, last_playoffs = TRUE, sleep = 10)
   f1 <- "./19751976.csv"
   f2 <- "./19751976Playoffs.csv"
   f3 <- "./wha19751976.csv"
   f4 <- "./wha19751976Playoffs.csv"
   f5 <- paste0("./scores-", Sys.Date(), ".RDS")
-  
+
   expect_equivalent(scraped_scores, sscores)
-  
+
   expect_true(file.exists(f1))
   expect_true(file.exists(f2))
   expect_true(file.exists(f3))
@@ -169,4 +176,21 @@ test_that("Scraper works", {
   try_delete(f3)
   try_delete(f4)
   try_delete(f5)
+})
+
+test_that("Schedule returns full cleaned season", {
+  fromdate <- paste0(getCurrentSeason()-1, "-09-01")
+  scraped_schedule <- getSchedule(fromdate, data_dir = "./", include_playoffs = FALSE)
+
+  f1 <- "./schedule.RDS"
+  expect_true(file.exists(f1))
+  try_delete(f1)
+
+  expect_equal(nrow(scraped_schedule), 1230)
+  expect_equal(unique(scraped_schedule$G), "")
+  expect_equal(unique(scraped_schedule$G.1), "")
+  expect_equal(unique(scraped_schedule$X), "")
+
+  fromdate <- paste0(getCurrentSeason() + 2, "-09-01")
+  expect_false(getSchedule(from_date = fromdate))
 })
