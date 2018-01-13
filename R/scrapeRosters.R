edf33bcfae3e98bb71034fdeece4b61ae9ba1be3
Philip Bulsink <pbulsink@users.noreply.github.com>
2018-01-13 11:43:20 -0500

    Dev (#8)
    * Made progress bars optional
    
    * Converted progress bars to progress:: from utils::
    
    * bug squashes
    
    * fixed & updated rosters
    
    * Update License Year

--- R/scrapeRosters.R
+++ R/scrapeRosters.R
@@ -1,25 +1,27 @@
-teamURLList <- data.frame(URL = c("http://www2.dailyfaceoff.com/teams/lines/13/anaheim-ducks/",
-  "http://www2.dailyfaceoff.com/teams/lines/35/arizona-coyotes", "http://www2.dailyfaceoff.com/teams/lines/15/boston-bruins/",
-  "http://www2.dailyfaceoff.com/teams/lines/16/buffalo-sabres/", "http://www2.dailyfaceoff.com/teams/lines/17/calgary-flames",
-  "http://www2.dailyfaceoff.com/teams/lines/18/carolina-hurricanes", "http://www2.dailyfaceoff.com/teams/lines/19/chicago-blackhawks",
-  "http://www2.dailyfaceoff.com/teams/lines/20/colorado-avalanche", "http://www2.dailyfaceoff.com/teams/lines/21/columbus-blue-jackets",
-  "http://www2.dailyfaceoff.com/teams/lines/22/dallas-stars", "http://www2.dailyfaceoff.com/teams/lines/23/detroit-red-wings",
-  "http://www2.dailyfaceoff.com/teams/lines/24/edmonton-oilers", "http://www2.dailyfaceoff.com/teams/lines/25/florida-panthers",
-  "http://www2.dailyfaceoff.com/teams/lines/26/los-angeles-kings", "http://www2.dailyfaceoff.com/teams/lines/27/minnesota-wild",
-  "http://www2.dailyfaceoff.com/teams/lines/28/montreal-canadiens", "http://www2.dailyfaceoff.com/teams/lines/29/nashville-predators",
-  "http://www2.dailyfaceoff.com/teams/lines/30/new-jersey-devils", "http://www2.dailyfaceoff.com/teams/lines/31/new-york-islanders",
-  "http://www2.dailyfaceoff.com/teams/lines/32/new-york-rangers", "http://www2.dailyfaceoff.com/teams/lines/33/ottawa-senators",
-  "http://www2.dailyfaceoff.com/teams/lines/34/philadelphia-flyers", "http://www2.dailyfaceoff.com/teams/lines/36/pittsburgh-penguins",
-  "http://www2.dailyfaceoff.com/teams/lines/37/san-jose-sharks", "http://www2.dailyfaceoff.com/teams/lines/38/st-louis-blues",
-  "http://www2.dailyfaceoff.com/teams/lines/39/tampa-bay-lightning", "http://www2.dailyfaceoff.com/teams/lines/40/toronto-maple-leafs",
-  "http://www2.dailyfaceoff.com/teams/lines/41/vancouver-canucks", "http://www2.dailyfaceoff.com/teams/lines/42/washington-capitals",
-  "http://www2.dailyfaceoff.com/teams/lines/14/winnipeg-jets"), Team = c("Anaheim Ducks", "Arizona Coyotes",
+teamURLList <- data.frame(URL = c("http://www.dailyfaceoff.com/teams/anaheim-ducks/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/arizona-coyotes/line-combinations/", "http://www.dailyfaceoff.com/teams/boston-bruins/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/buffalo-sabres/line-combinations/", "http://www.dailyfaceoff.com/teams/calgary-flames/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/carolina-hurricanes/line-combinations/", "http://www.dailyfaceoff.com/teams/chicago-blackhawks/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/colorado-avalanche/line-combinations/", "http://www.dailyfaceoff.com/teams/columbus-blue-jackets/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/dallas-stars/line-combinations/", "http://www.dailyfaceoff.com/teams/detroit-red-wings/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/edmonton-oilers/line-combinations/", "http://www.dailyfaceoff.com/teams/florida-panthers/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/los-angeles-kings/line-combinations/", "http://www.dailyfaceoff.com/teams/minnesota-wild/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/montreal-canadiens/line-combinations/", "http://www.dailyfaceoff.com/teams/nashville-predators/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/new-jersey-devils/line-combinations/", "http://www.dailyfaceoff.com/teams/new-york-islanders/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/new-york-rangers/line-combinations/", "http://www.dailyfaceoff.com/teams/ottawa-senators/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/philadelphia-flyers/line-combinations/", "http://www.dailyfaceoff.com/teams/pittsburgh-penguins/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/san-jose-sharks/line-combinations/", "http://www.dailyfaceoff.com/teams/st-louis-blues/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/tampa-bay-lightning/line-combinations/", "http://www.dailyfaceoff.com/teams/toronto-maple-leafs/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/vancouver-canucks/line-combinations/", "http://www.dailyfaceoff.com/teams/washington-capitals/line-combinations/",
+  "http://www.dailyfaceoff.com/teams/winnipeg-jets/line-combinations/", "http://www.dailyfaceoff.com/teams/vegas-golden-knights/line-combinations/"), 
+                          Team = c("Anaheim Ducks", "Arizona Coyotes",
   "Boston Bruins", "Buffalo Sabres", "Calgary Flames", "Carolina Hurricanes", "Chicago Blackhawks",
   "Colorado Avalanche", "Columbus Blue Jackets", "Dallas Stars", "Detroit Red Wings", "Edmonton Oilers",
   "Florida Panthers", "Los Angeles Kings", "Minnesota Wild", "Montreal Canadiens", "Nashville Predators",
   "New Jersey Devils", "New York Islanders", "New York Rangers", "Ottawa Senators", "Philadelphia Flyers",
   "Pittsburgh Penguins", "San Jose Sharks", "St. Louis Blues", "Tampa Bay Lightning", "Toronto Maple Leafs",
-  "Vancouver Canucks", "Washington Capitals", "Winnipeg Jets"), stringsAsFactors = FALSE)
+  "Vancouver Canucks", "Washington Capitals", "Winnipeg Jets", "Vegas Golden Knights"), stringsAsFactors = FALSE)
+
 
 #' Get Current estimated rosters
 #'
@@ -31,27 +33,39 @@ teamURLList <- data.frame(URL = c("http://www2.dailyfaceoff.com/teams/lines/13/a
 #' \item{Team}{The team}
 #' \item{Player}{The name of the Forward, Defence or Goalie playing}
 #' \item{updateDate}{The date the Team's page was updated}
+#' @export
+
 getCurrentRosters <- function(sleep = 30, teamUrlList = teamURLList, progress=TRUE) {
   rosters <- data.frame(Team = character(), Players = character(), updateDate = character())
 
   if(progress){
-  pb <- utils::txtProgressBar(min = 0, max = nrow(teamUrlList), initial = 0)
+    pb <- progress::progress_bar$new(
+      format = "  downloading rosters [:bar] :percent eta: :eta",
+      clear = FALSE,
+      width = 80,
+      total=nrow(teamUrlList)
+    )
+    pb$tick(0)
   }
 
   for (i in 1:nrow(teamUrlList)) {
-    htmlpage <- getURLInternal(teamUrlList[i, 1], referer = "http://www2.dailyfaceoff.com/")
-    tabs <- XML::readHTMLTable(htmlpage, header = FALSE)
-    pattern <- "Last update: ([A-Za-z0-9\\., ]+)<\\/div>"
-    dt <- as.character(as.Date(gsub("\\.", "", stringr::str_match(htmlpage, pattern)[1, 2]),
+    htmlpage <- getURLInternal(teamUrlList[i, 1], referer = "http://www.dailyfaceoff.com/")
+    tabs <- XML::readHTMLTable(htmlpage, header = FALSE, stringsAsFactors=FALSE)
+    date_pattern <- "Last update: ([A-Za-z0-9\\., ]+)<\\/div>"
+    dt <- as.character(as.Date(gsub("\\.", "", stringr::str_match(htmlpage, date_pattern)[1, 2]),
       format = "%b%t%e,%t%Y"))
-    r <- data.frame(Team = rep(teamUrlList[i, 2]), Players = c(levels(unlist(tabs$forwards)),
-      levels(unlist(tabs$defense)), levels(unlist(tabs$goalie_list))), updateDate = rep(dt),
+    player_pattern<-"\\$[0-9]+([A-Za-z\\s]+)$"
+    r <- data.frame(Team = rep(teamUrlList[i, 2]),
+                    Players = c(stringr::str_match(unlist(tabs$forwards), player_pattern)[,2],
+                                stringr::str_match(unlist(tabs$defense), player_pattern)[,2],
+                                stringr::str_match(unlist(tabs$goalie_list), player_pattern)[,2]),
+                    updateDate = rep(dt),
       stringsAsFactors = FALSE)
     rosters <- rbind(rosters, r)
-    if(progress){
-      utils::setTxtProgressBar(pb, i)
-    }
     Sys.sleep(sleep)
+    if(progress){
+      pb$tick()
+    }
   }
 
   rosters$Team <- factor(rosters$Team)
