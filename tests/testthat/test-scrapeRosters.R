e7129e2c68f57ebf92f1a85aa39a7c03843460f7
Phil OSx <bulsinkp@gmail.com>
2018-02-24 16:01:29 -0500

    extra test for scrape rosters.

--- tests/testthat/test-scrapeRosters.R
+++ tests/testthat/test-scrapeRosters.R
@@ -7,7 +7,7 @@ test_that("Getting and parsing rosters URL page works", {
     expect_named(roster, "ANA")
     expect_named(roster$ANA, c("Forwards", "Defence", "Goalies", "PP1", "PP2", "Injuries", "UpdateDate"))
     # Teams can dress up to 23 active players in a game
-    expect_less_than(sum(length(roster$ANA$Forwards),length(roster$ANA$Defence), length(roster$ANA$Goalies)), 24)
+    expect_lte(sum(length(roster$ANA$Forwards),length(roster$ANA$Defence), length(roster$ANA$Goalies)), 23)
     # Teams have 2 power play lines of 5 each
     expect_length(roster$ANA$PP1, 5)
     expect_length(roster$ANA$PP2, 5)
@@ -21,7 +21,7 @@ test_that("Scraping Rosters Works", {
     #All sublists should exist even if no data (ie no one on IR)
     expect_named(roster$ANA, c("Forwards", "Defence", "Goalies", "PP1", "PP2", "Injuries", "UpdateDate"))
     # Teams can dress up to 23 active players in a game
-    expect_less_than(sum(length(roster$ANA$Forwards),length(roster$ANA$Defence), length(roster$ANA$Goalies)), 24)
+    expect_lte(sum(length(roster$ANA$Forwards),length(roster$ANA$Defence), length(roster$ANA$Goalies)), 23)
     # Teams have 2 power play lines of 5 each
     expect_length(roster$ANA$PP1, 5)
     expect_length(roster$ANA$PP2, 5)
@@ -30,3 +30,8 @@ test_that("Scraping Rosters Works", {
     expect_true(file.exists(f1))
     try_delete(f1)
 })
+
+test_that("Bad Teams get caught", {
+  expect_error(getCurrentRosters(sleep=0, team="Bob", progress=FALSE), NULL)
+})
+
