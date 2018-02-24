d2fcaec8c7e189396d89346c1312d859a0c14c79
Philip Bulsink <pbulsink@users.noreply.github.com>
2018-02-24 16:25:21 -0500

    Dev (#13)
    * modified scrapeRoster to take a list of teams to scrape (or null for all) instead of a df of teams & urls
    
    * Added scraping rosters to vignette
    
    * Link to vignette in readme. Bump 0.2.1
    
    * extra test for scrape rosters.

---
+++ vignettes/HockeyScrapR.R
@@ -0,0 +1,39 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----install, eval=FALSE-------------------------------------------------
+#  install.packages('devtools')
+#  
+#  devtools::install_github(pbulsink/HockeyScrapR)
+
+## ----library-------------------------------------------------------------
+library(HockeyScrapR)
+
+## ----getrosters----------------------------------------------------------
+teamTOR <- data.frame(
+  URL = "http://www.dailyfaceoff.com/teams/toronto-maple-leafs/line-combinations/", 
+  Team="TOR", 
+  stringsAsFactors = FALSE)
+roster<-getCurrentRosters(teamUrlList = teamTOR, sleep = 0, progress = FALSE)
+
+## ----seeRosterForwards---------------------------------------------------
+roster$TOR$Forwards
+
+## ----seeRosterGoalies----------------------------------------------------
+roster$TOR$Goalies
+
+## ----seeRostersInjury----------------------------------------------------
+roster$TOR$Injury
+
+## ----seeRosterUpdate-----------------------------------------------------
+roster$TOR$UpdateDate
+
+## ----seeRostersList------------------------------------------------------
+names(roster$TOR)
+
+## ----saveRoster, eval=FALSE----------------------------------------------
+#  roster<-scrapeRosters(data_dir = "./data/rosters/", teamUrlList = teamTOR, sleep = 0, progress = FALSE)
+
