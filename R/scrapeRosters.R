bf8f8a19bbfdafe83ddfd0c2d9f77593ce0803e1
Philip Bulsink <pbulsink@users.noreply.github.com>
2018-02-24 08:33:34 -0500

    Improve roster scraping to include power play units and injury reserve, plus bugfixes (#12)
    * Made progress bars optional
    
    * Converted progress bars to progress:: from utils::
    
    * bug squashes
    
    * fixed & updated rosters
    
    * Update License Year
    
    * Add vignettes to the package.
    
    * renamed vignette to no spaces (passes check)
    
    * Some work on scrapeRosters to get multi-name players scraped properly
    
    * Trying to add powerplay units. Won't pass tests.
    
    * Found a data hosting format I liked, changed team names to abbreviations
    
    * took out an old garbage line.
    
    * Rosters scrapes Powerplay lines, injuries, and names with special characters, hyphens and periods (Bob Ste. Hénri-Descartes would scrape properly). Updated tests

--- R/scrapeRosters.R
+++ R/scrapeRosters.R
@@ -13,15 +13,9 @@ teamURLList <- data.frame(URL = c("http://www.dailyfaceoff.com/teams/anaheim-duc
   "http://www.dailyfaceoff.com/teams/san-jose-sharks/line-combinations/", "http://www.dailyfaceoff.com/teams/st-louis-blues/line-combinations/",
   "http://www.dailyfaceoff.com/teams/tampa-bay-lightning/line-combinations/", "http://www.dailyfaceoff.com/teams/toronto-maple-leafs/line-combinations/",
   "http://www.dailyfaceoff.com/teams/vancouver-canucks/line-combinations/", "http://www.dailyfaceoff.com/teams/washington-capitals/line-combinations/",
-  "http://www.dailyfaceoff.com/teams/winnipeg-jets/line-combinations/", "http://www.dailyfaceoff.com/teams/vegas-golden-knights/line-combinations/"), 
-                          Team = c("Anaheim Ducks", "Arizona Coyotes",
-  "Boston Bruins", "Buffalo Sabres", "Calgary Flames", "Carolina Hurricanes", "Chicago Blackhawks",
-  "Colorado Avalanche", "Columbus Blue Jackets", "Dallas Stars", "Detroit Red Wings", "Edmonton Oilers",
-  "Florida Panthers", "Los Angeles Kings", "Minnesota Wild", "Montreal Canadiens", "Nashville Predators",
-  "New Jersey Devils", "New York Islanders", "New York Rangers", "Ottawa Senators", "Philadelphia Flyers",
-  "Pittsburgh Penguins", "San Jose Sharks", "St. Louis Blues", "Tampa Bay Lightning", "Toronto Maple Leafs",
-  "Vancouver Canucks", "Washington Capitals", "Winnipeg Jets", "Vegas Golden Knights"), stringsAsFactors = FALSE)
-
+  "http://www.dailyfaceoff.com/teams/winnipeg-jets/line-combinations/", "http://www.dailyfaceoff.com/teams/vegas-golden-knights/line-combinations/"),
+                          Team = c("ANA", "ARI", "BOS", "BUF", "CGY", "CAR", "CHI", "COL", "CBJ", "DAL", "DET", "EDM",
+  "FLA", "LAK", "MIN", "MTL", "NSH","NJD", "NYI", "NYR", "OTT", "PHI", "PIT", "SJS", "STL", "TBL", "TOR", "VAN", "WSH", "WPG", "VGK"), stringsAsFactors = FALSE)
 
 #' Get Current estimated rosters
 #'
@@ -29,15 +23,18 @@ teamURLList <- data.frame(URL = c("http://www.dailyfaceoff.com/teams/anaheim-duc
 #' @param teamUrlList A list of team roster page urls on dailyfaceoff.com. Defaults to correct team URLs.
 #' @param progress Whether to show a progress bar. Default = TRUE.
 #'
-#' @return a long data frame with three columns:
-#' \item{Team}{The team}
-#' \item{Player}{The name of the Forward, Defence or Goalie playing}
+#' @return a list of vectors by team, with each team's list named:
+#' \item{Forwards}{The names of the Forwards playing}
+#' \item{Defence}{The names of the Defence playing}
+#' \item{Goalies}{The names of the Goalies playing}
+#' \item{PP1}{The first powerplay unit}
+#' \item{PP2}{The second powerplay unit}
+#' \item{Injuries}{Any players on the injury reserve}
 #' \item{updateDate}{The date the Team's page was updated}
 #' @export
 
 getCurrentRosters <- function(sleep = 30, teamUrlList = teamURLList, progress=TRUE) {
   rosters <- data.frame(Team = character(), Players = character(), updateDate = character())
-
   if(progress){
     pb <- progress::progress_bar$new(
       format = "  downloading rosters [:bar] :percent eta: :eta",
@@ -48,28 +45,44 @@ getCurrentRosters <- function(sleep = 30, teamUrlList = teamURLList, progress=TR
     pb$tick(0)
   }
 
+  nteams<-nrow(teamUrlList)
+  rosters<-vector(mode = 'list', length=nteams)
+  names(rosters)<-teamUrlList[,2]
+  date_pattern <- "Last update: ([A-Za-z0-9\\., ]+)<\\/div>"
+  #Note: includes special characters
+  player_pattern<-"\\$[0-9]+([A-z\u00c0-\u00ff\\s.\\-]+)$"
+
   for (i in 1:nrow(teamUrlList)) {
     htmlpage <- getURLInternal(teamUrlList[i, 1], referer = "http://www.dailyfaceoff.com/")
     tabs <- XML::readHTMLTable(htmlpage, header = FALSE, stringsAsFactors=FALSE)
-    date_pattern <- "Last update: ([A-Za-z0-9\\., ]+)<\\/div>"
+
     dt <- as.character(as.Date(gsub("\\.", "", stringr::str_match(htmlpage, date_pattern)[1, 2]),
       format = "%b%t%e,%t%Y"))
-    player_pattern<-"\\$[0-9]+([A-Za-z\\s]+)$"
-    r <- data.frame(Team = rep(teamUrlList[i, 2]),
-                    Players = c(stringr::str_match(unlist(tabs$forwards), player_pattern)[,2],
-                                stringr::str_match(unlist(tabs$defense), player_pattern)[,2],
-                                stringr::str_match(unlist(tabs$goalie_list), player_pattern)[,2]),
-                    updateDate = rep(dt),
-      stringsAsFactors = FALSE)
-    rosters <- rbind(rosters, r)
+    f <- stringr::str_match(unlist(tabs$forwards), player_pattern)[,2]
+    d <- stringr::str_match(unlist(tabs$defense), player_pattern)[,2]
+    g <- stringr::str_match(unlist(tabs$goalie_list), player_pattern)[,2]
+    p1 <- c(stringr::str_match(unlist(tabs[3]), player_pattern)[,2], stringr::str_match(unlist(tabs[4]), player_pattern)[,2])
+    p2 <- c(stringr::str_match(unlist(tabs[5]), player_pattern)[,2], stringr::str_match(unlist(tabs[6]), player_pattern)[,2])
+    ij <- stringr::str_match(unlist(tabs[8]), player_pattern)[,2]
+    #Making sure no NA make it to the rosters.
+    f <- f[!is.na(f)]
+    d <- d[!is.na(d)]
+    g <- g[!is.na(g)]
+    p1 <- p1[!is.na(p1)]
+    p2 <- p2[!is.na(p2)]
+    ij <- ij[!is.na(ij)]
+    if(length(ij) == 0){
+      ij<-""
+    }
+
+    rosters[[i]] <- list(Forwards = f, Defence = d, Goalies = g, PP1 = p1,
+                       PP2 = p2, Injuries = ij, UpdateDate = as.Date(dt))
+
     Sys.sleep(sleep)
     if(progress){
       pb$tick()
     }
   }
-
-  rosters$Team <- factor(rosters$Team)
-  rosters$updateDate <- as.Date(rosters$updateDate)
   return(rosters)
 }
 
@@ -80,9 +93,9 @@ getCurrentRosters <- function(sleep = 30, teamUrlList = teamURLList, progress=TR
 #' Also saves results to a dated .RDS file.
 #'
 #' @param data_dir Directory to store data in
-#' @param ... Additional parameters to pass
+#' @param ... Additional parameters to pass to getCurrentRosters
 #'
-#' @return roster information data.frame
+#' @return roster information list
 #' @export
 #'
 #' @examples
@@ -91,6 +104,9 @@ getCurrentRosters <- function(sleep = 30, teamUrlList = teamURLList, progress=TR
 #' }
 scrapeRosters <- function(data_dir = "./data/rosters/", ...) {
   rosters <- getCurrentRosters(...)
+  if(!dir.exists(data_dir)){
+    dir.create(data_dir, recursive = TRUE)
+  }
   saveRDS(rosters, paste0(data_dir, "rosters-", Sys.Date(), ".RDS"))
   return(rosters)
 }
--- tests/testthat/test-scrapeRosters.R
+++ tests/testthat/test-scrapeRosters.R
@@ -3,23 +3,33 @@ source("helper-scrapeRosters.R")
 
 test_that("Getting and parsing rosters URL page works", {
     teamURLList <- data.frame(URL = c("http://www.dailyfaceoff.com/teams/anaheim-ducks/line-combinations/"),
-        Team = c("Anaheim Ducks"), stringsAsFactors = FALSE)
-    roster <- getCurrentRosters(sleep = 5, teamUrlList = teamURLList)
+        Team = c("ANA"), stringsAsFactors = FALSE)
+    roster <- getCurrentRosters(sleep = 0, teamUrlList = teamURLList, progress = FALSE)
 
-    expect_named(roster, c("Team", "Players", "updateDate"))
-    # Teams can dress up to 20 active players in a game
-    expect_equal(nrow(roster), 20)
+    expect_named(roster, "ANA")
+    expect_named(roster$ANA, c("Forwards", "Defence", "Goalies", "PP1", "PP2", "Injuries", "UpdateDate"))
+    # Teams can dress up to 23 active players in a game
+    expect_less_than(sum(length(roster$ANA$Forwards),length(roster$ANA$Defence), length(roster$ANA$Goalies)), 24)
+    # Teams have 2 power play lines of 5 each
+    expect_length(roster$ANA$PP1, 5)
+    expect_length(roster$ANA$PP2, 5)
 })
 
 test_that("Scraping Rosters Works", {
     teamURLList <- data.frame(URL = c("http://www.dailyfaceoff.com/teams/anaheim-ducks/line-combinations/"),
-        Team = c("Anaheim Ducks"), stringsAsFactors = FALSE)
-    roster <- scrapeRosters(data_dir = "./", sleep = 5, teamUrlList = teamURLList)
+        Team = c("ANA"), stringsAsFactors = FALSE)
+    roster <- scrapeRosters(data_dir = "./", sleep = 0, teamUrlList = teamURLList, progress=FALSE)
 
-    expect_named(roster, c("Team", "Players", "updateDate"))
-    # Teams can dress up to 20 active players in a game.
-    expect_true(nrow(roster) <= 20)
-    expect_true(nchar(roster$Players[1]) > 3)
+    #team name should be list key
+    expect_named(roster, "ANA")
+    #All sublists should exist even if no data (ie no one on IR)
+    expect_named(roster$ANA, c("Forwards", "Defence", "Goalies", "PP1", "PP2", "Injuries", "UpdateDate"))
+    # Teams can dress up to 23 active players in a game
+    expect_less_than(sum(length(roster$ANA$Forwards),length(roster$ANA$Defence), length(roster$ANA$Goalies)), 24)
+    # Teams have 2 power play lines of 5 each
+    expect_length(roster$ANA$PP1, 5)
+    expect_length(roster$ANA$PP2, 5)
+    #File should be written
     f1 <- paste0("./rosters-", Sys.Date(), ".RDS")
     expect_true(file.exists(f1))
     try_delete(f1)
